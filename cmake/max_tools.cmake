ce8bc434ec9f8b50da0e882aed2419680fa8522e
Bijesh <bijesh.poyil@analog.com>
2025-07-14 14:20:40 +0530

    Added seperate files for st and max tools

---
+++ cmake/max_tools.cmake
@@ -0,0 +1,3 @@
+#Update this based on instalation path
+set(SDK C:/MaximSDK CACHE PATH "Path to MAXIM_DRIVERS directory")
+set(TOOLCHAIN_PATH "${SDK}/Tools/GNUTools/10.3/bin" CACHE PATH "Toolchain path")
---
+++ cmake/st_tools.cmake
@@ -0,0 +1,3 @@
+#Update this if required. STM32CLT_PATH is normally set during installation of STM32CLT
+set(TOOLCHAIN_PATH "${STM32CLT_PATH}/GNU-tools-for-STM32/bin" CACHE PATH "Toolchain path")
+set(SDK C:/ST/STM32CubeH5/Drivers/STM32H5xx_HAL_Driver CACHE PATH "Path to STM32H5 Drivers directory")
