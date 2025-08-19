aa9a129a66e66b8c8aa244e0762242f06af9a90a
LayaSree <LayaSree.Nuthanapati@analog.com>
2025-08-19 12:54:02 +0530

    resolved build errors in maxim proj

--- cmake/max_tools.cmake
+++ cmake/max_tools.cmake
@@ -1,4 +1,5 @@
 #Update this based on instalation path
+set(SDK C:/MaximSDK CACHE PATH "Path to MAXIM_DRIVERS directory")
 set(ARM_GCC_TOOLCHAIN_PATH "${SDK}/Tools/GNUTools/10.3/bin" CACHE PATH "ARm Gcc Toolchain path")
 set(ARM_CLANG_TOOLCHAIN_PATH "C:/Keil_v5/ARM/ARMCLANG/Bin" CACHE PATH "Keil Toolchain path")
 if(TOOLCHAIN MATCHES "gcc-arm-none-eabi")
@@ -6,4 +7,3 @@ if(TOOLCHAIN MATCHES "gcc-arm-none-eabi")
 elseif(TOOLCHAIN MATCHES "armclang")
 set(TOOLCHAIN_PATH ${ARM_CLANG_TOOLCHAIN_PATH} CACHE PATH "Toolchain Path")
 endif()
-set(SDK C:/MaximSDK CACHE PATH "Path to MAXIM_DRIVERS directory")
